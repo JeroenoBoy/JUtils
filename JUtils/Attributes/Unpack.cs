3ccb28a6ae5e0acf4970f099c374993d1a6ff31d
Jeroen <jeroen@vandegeest.eu>
2022-10-30 16:57:03 +0100

    Fixed issue with Unpack

--- JUtils/Attributes/Unpack.cs
+++ JUtils/Attributes/Unpack.cs
@@ -15,10 +15,11 @@ namespace JUtils.Attributes
             {
                 property.NextVisible(true);
                 float height = 0;
+                int depth = property.depth;
 
                 do {
                     height += EditorGUI.GetPropertyHeight(property) + 2;
-                } while (property.NextVisible(false));
+                } while (property.NextVisible(false) && property.depth == depth);
 
                 return height;
             }
@@ -27,13 +28,14 @@ namespace JUtils.Attributes
             public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
             {
                 property.NextVisible(true);
+                int depth = property.depth;
 
                 do {
                     float height = position.height = EditorGUI.GetPropertyHeight(property);
                     EditorGUI.PropertyField(position, property, true);
                     position.y += height + 2;
 
-                } while (property.NextVisible(false));
+                } while (property.NextVisible(false) && property.depth == depth);
             }
         }
 #endif
