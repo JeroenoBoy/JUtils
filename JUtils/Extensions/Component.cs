6afc5669dda721fd3c0f41ccd36662c20fa4e36f
Jeroen <jeroen@vandegeest.eu>
2022-11-09 10:11:24 +0100

    Added copy utility for components

--- JUtils/Extensions/Component.cs
+++ JUtils/Extensions/Component.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Reflection;
+using System;
+using UnityEngine;
 
 
 
@@ -23,6 +25,36 @@ namespace JUtils.Extensions
         {
             GameObject gameObject = comp.gameObject;
             return ((1 << gameObject.layer) & layer) != 0 || layer == gameObject.layer;
+        }
+        
+        
+        /// <summary>
+        /// Copy one component to another
+        /// Source: https://answers.unity.com/questions/530178/how-to-get-a-component-from-an-object-and-add-it-t.html
+        /// </summary>
+        /// <param name="comp"></param>
+        /// <param name="other"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T GetCopyOf<T>(this Component comp, T other) where T : Component
+        {
+            Type type = comp.GetType();
+            if (type != other.GetType()) return null; // type mis-match
+            BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default | BindingFlags.DeclaredOnly;
+            PropertyInfo[] pinfos = type.GetProperties(flags);
+            foreach (var pinfo in pinfos) {
+                if (pinfo.CanWrite) {
+                    try {
+                        pinfo.SetValue(comp, pinfo.GetValue(other, null), null);
+                    }
+                    catch { } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
+                }
+            }
+            FieldInfo[] finfos = type.GetFields(flags);
+            foreach (var finfo in finfos) {
+                finfo.SetValue(comp, finfo.GetValue(other));
+            }
+            return comp as T;
         }
     }
 }
