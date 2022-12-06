fd442e2e7e13d19d66e531cbc65d25fa22baeca9
Jeroen <jeroen@vandegeest.eu>
2022-12-06 09:59:37 +0100

    Added SerializeInterface

---
+++ JUtils/Attributes/SerializeInterface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+
+
+namespace JUtils.Attributes
+{
+    public class SerializeInterface : PropertyAttribute
+    {
+        private readonly Type _type;
+        public SerializeInterface(Type type)
+        {
+            _type = type;
+        }
+
+
+        
+#if UNITY_EDITOR
+        [CustomPropertyDrawer(typeof(SerializeInterface))]
+        public class SerializeInterfaceEditor : PropertyDrawer
+        {
+            public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+            {
+                SerializeInterface target = attribute as SerializeInterface;
+                if (property.type != "PPtr<$Object>") throw new Exception("Field must be of type Object");
+            
+                property.objectReferenceValue = EditorGUI.ObjectField(position, label, property.objectReferenceValue, target._type, true);
+            }
+        }
+#endif
+    }
+}
