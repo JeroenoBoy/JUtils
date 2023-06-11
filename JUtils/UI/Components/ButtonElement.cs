3e901ee416721d90c46f162fb9a1957f2a1bca55
Jeroen <jeroen@vandegeest.eu>
2023-06-11 17:07:09 +0200

    Add ButtonElement and TextFieldElement

---
+++ JUtils/UI/Components/ButtonElement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine.UIElements;
+
+
+
+namespace JUtils.UI
+{
+    /// <summary>
+    /// This class provides callbacks for working with button elements
+    /// </summary>
+    /// <example><code lang="CSharp">
+    /// namespace Example
+    /// {
+    ///     public class QuitGameButton : ButtonElement
+    ///     {
+    ///         public override void OnClick(ClickEvent e)
+    ///         {
+    ///             Debug.Log("Quitting!");
+    ///             Application.Quit();
+    ///         }
+    ///     }
+    /// }
+    /// </code></example>
+    public abstract class ButtonElement : UIElement<Button>
+    {
+        public override void Activate(VisualElement element)
+        {
+            if (active) return;
+            base.Activate(element);
+
+            Button button = this.element;
+            button.RegisterCallback<ClickEvent>(OnClick);
+            button.RegisterCallback<FocusInEvent>(OnFocusIn);
+            button.RegisterCallback<FocusOutEvent>(OnFocusOut);
+        }
+
+
+        protected abstract void OnClick(ClickEvent e);
+        protected virtual void OnFocusIn(FocusInEvent e) {}
+        protected virtual void OnFocusOut(FocusOutEvent e) {}
+    }
+}
---
+++ JUtils/UI/Components/TextFieldElement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.UIElements;
+
+
+
+namespace JUtils.UI
+{
+    /// <summary>
+    /// This class provides callbacks for working with textfield elements
+    /// </summary>
+    /// <example><code lang="CSharp">
+    /// namespace Example
+    /// {
+    ///     public class NameField : InputElement
+    ///     {
+    ///         public override void OnClick(ClickEvent e)
+    ///         {
+    ///             Debug.Log("Quitting!");
+    ///             Application.Quit();
+    ///         }
+    ///     }
+    /// }
+    /// </code></example>
+    public abstract class TextFieldElement : UIElement<TextField>
+    {
+        public override void Activate(VisualElement element)
+        {
+            if (active) return;
+            base.Activate(element);
+
+            TextField textField = this.element;
+            textField.RegisterCallback<NavigationSubmitEvent>(OnSubmit);
+            textField.RegisterCallback<InputEvent>(OnInput);
+            textField.RegisterCallback<FocusInEvent>(OnFocusIn);
+            textField.RegisterCallback<FocusOutEvent>(OnFocusOut);
+        }
+
+
+        protected abstract void OnSubmit(NavigationSubmitEvent e);
+        protected virtual  void OnInput(InputEvent e) { }
+        protected virtual  void OnFocusIn(FocusInEvent e) { }
+        protected virtual  void OnFocusOut(FocusOutEvent e) { }
+    }
+}
