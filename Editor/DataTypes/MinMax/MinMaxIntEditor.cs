50f3f9c20e7d1bd9b174e652fad10b80b2cc012d
Jeroen <jeroen@vandegeest.eu>
2023-11-14 21:07:54 +0100

    Convert MinMax editor to visual elements

---
+++ Editor/DataTypes/MinMax/MinMaxIntEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace JUtils.Editor
+{
+    [CustomPropertyDrawer(typeof(MinMaxInt))]
+    public class MinMaxIntEditor : BaseMinMaxEditor
+    {
+        protected override BindableElement CreateElement(string name)
+        {
+            return new IntegerField(name);
+        }
+    }
+}
