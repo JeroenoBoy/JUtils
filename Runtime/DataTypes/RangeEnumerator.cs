d6106a5ec2d6c57ac89af304d43c2ac7f36fb549
Jeroen <jeroen@vandegeest.eu>
2024-02-08 10:43:42 +0100

    Conventions

--- Runtime/DataTypes/RangeEnumerator.cs
+++ Runtime/DataTypes/RangeEnumerator.cs
@@ -24,14 +24,22 @@ namespace JUtils
     ///         }
     ///     }
     /// }
-    /// </code></example>>
+    /// </code></example>
     public static class RangeExtensions
     {
-        public static RangeEnumerator GetEnumerator(this Range range) => new (range);
-        public static RangeEnumerator GetEnumerator(this int end) => new (0, end);
+        public static RangeEnumerator GetEnumerator(this Range range)
+        {
+            return new RangeEnumerator(range);
+        }
+
+
+        public static RangeEnumerator GetEnumerator(this int end)
+        {
+            return new RangeEnumerator(0, end);
+        }
     }
-    
-    
+
+
     /// <summary>
     /// Allows for complex enumerations for Range types
     /// </summary>
@@ -40,7 +48,7 @@ namespace JUtils
         private readonly int _startIndex;
         private readonly int _direction = 1;
         private readonly int _end;
-        
+
         private int _index;
 
 
@@ -51,21 +59,15 @@ namespace JUtils
                     _direction = -1;
                     _startIndex = _index = range.Start.Value;
                     _end = range.End.Value;
+                } else {
+                    _startIndex = _index = range.Start.Value - 1;
+                    _end = range.End.Value - 1;
                 }
-                else {
-                    _startIndex = _index = range.Start.Value-1;
-                    _end = range.End.Value-1;
-                }
-                
-            }
-
-            else if (range.End.IsFromEnd) {
+            } else if (range.End.IsFromEnd) {
                 _direction = -1;
-                _startIndex = _index = range.Start.Value+1;
+                _startIndex = _index = range.Start.Value + 1;
                 _end = range.End.Value;
-            }
-
-            else {
+            } else {
                 _startIndex = _index = range.Start.Value - 1;
                 _end = range.End.Value;
             }
@@ -80,13 +82,21 @@ namespace JUtils
         {
             if (start > end) throw new ArgumentException("Start must not be greater than end");
 
-            _startIndex = _index = start-1;
-            _end = end-1;
+            _startIndex = _index = start - 1;
+            _end = end - 1;
         }
 
 
-        public bool MoveNext() => _direction == 1 ? _index++ < _end : _index --> _end;
-        public void Reset() => _index = _startIndex;
+        public bool MoveNext()
+        {
+            return _direction == 1 ? _index++ < _end : _index-- > _end;
+        }
+
+
+        public void Reset()
+        {
+            _index = _startIndex;
+        }
 
 
         public int Current => _index;
