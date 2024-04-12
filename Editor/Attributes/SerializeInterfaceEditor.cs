2eacbb58346eefaeab4133ff8d119550f69a2f2d
Jeroen <jeroen@vandegeest.eu>
2024-04-12 19:43:50 +0200

    Fix SerializeInterface not saving

--- Editor/Attributes/SerializeInterfaceEditor.cs
+++ Editor/Attributes/SerializeInterfaceEditor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using UnityEditor;
 using UnityEditor.UIElements;
-using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace JUtils.Editor
@@ -10,26 +9,15 @@ namespace JUtils.Editor
     public class SerializeInterfaceEditor : PropertyDrawer
     {
         private new SerializeInterfaceAttribute attribute => base.attribute as SerializeInterfaceAttribute;
-        
+
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
             if (property.type != "PPtr<$Object>") throw new Exception("Field must be of type Object");
 
-            ObjectField field = new(property.displayName) {
-                objectType = attribute.type,
-                allowSceneObjects = true,
-                value = property.objectReferenceValue
-            };
+            ObjectField field = new(property.displayName) { objectType = attribute.type, allowSceneObjects = true, value = property.objectReferenceValue };
+            field.BindProperty(property);
 
-            field.RegisterValueChangedCallback(e => property.objectReferenceValue = e.newValue);
             return field;
-        }
-
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            SerializeInterfaceAttribute target = attribute;
-        
-            property.objectReferenceValue = EditorGUI.ObjectField(position, label, property.objectReferenceValue, target.type, true);
         }
     }
 }
