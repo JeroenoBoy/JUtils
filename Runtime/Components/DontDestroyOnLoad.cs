7bd5d8e5b0ebc648a907be80e0025ef6c1fe9516
Jeroen <jeroen@vandegeest.eu>
2024-05-25 12:48:32 +0200

    Seal classes that should not be inherited from

--- Runtime/Components/DontDestroyOnLoad.cs
+++ Runtime/Components/DontDestroyOnLoad.cs
@@ -5,7 +5,7 @@ namespace JUtils
     /// <summary>
     /// Runs the DDOL function on this gameobject, this is a more declerative way to mark an object for DDOL
     /// </summary>
-    public class DontDestroyOnLoad : MonoBehaviour
+    public sealed class DontDestroyOnLoad : MonoBehaviour
     {
         private void Awake()
         {
