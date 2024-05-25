3dd4f17fd1e792282d7429c47cf846aaf6adf8c9
Jeroen <jeroen@vandegeest.eu>
2024-05-25 12:51:21 +0200

    Update WeightedExtensions to use IList instead of Array as paramters, and rename Random to WeightedRandom

--- Runtime/Extensions/WeightedExtensions.cs
+++ Runtime/Extensions/WeightedExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace JUtils
+﻿using System;
+using System.Collections.Generic;
+
+namespace JUtils
 {
     /// <summary>
     /// Contains helper functions for working with Weighted Randomness
@@ -8,54 +11,50 @@
         /// <summary>
         /// Get a random weighted value using UnityEngine.Random
         /// </summary>
-        public static T Random<T>(this Weighted<T>[] weightedArray)
+        public static T WeightedRandom<T>(this IList<Weighted<T>> weightedArray)
         {
             float sum = weightedArray.WeightedSum();
             float index = UnityEngine.Random.Range(0, sum);
             return weightedArray.GetWeightedValue(index).value;
         }
 
-
         /// <summary>
         /// Get a random weighted value using System.Random
         /// </summary>
-        public static T Random<T>(this Weighted<T>[] weightedArray, System.Random random)
+        public static T WeightedRandom<T>(this IList<Weighted<T>> weightedArray, Random random)
         {
             float sum = weightedArray.WeightedSum();
             float index = (float)(random.NextDouble() * sum);
             return weightedArray.GetWeightedValue(index).value;
         }
-
 
         /// <summary>
         /// Get a random weighted value using UnityEngine.Random
         /// </summary>
-        public static T Random<T>(this T[] weightedArray) where T : IWeighted
+        public static T WeightedRandom<T>(this IList<T> weightedArray) where T : IWeighted
         {
             float sum = weightedArray.WeightedSum();
             float index = UnityEngine.Random.Range(0, sum);
             return weightedArray.GetWeightedValue(index);
         }
-        
-        
+
         /// <summary>
         /// Get a random weighted value using System.Random
         /// </summary>
-        public static T Random<T>(this T[] weightedArray, System.Random random) where T : IWeighted
+        public static T WeightedRandom<T>(this IList<T> weightedArray, Random random) where T : IWeighted
         {
             float sum = weightedArray.WeightedSum();
             float index = (float)(random.NextDouble() * sum);
             return weightedArray.GetWeightedValue(index);
         }
 
-
         /// <summary>
         /// Get a weighted value at a certain index
         /// </summary>
-        public static T GetWeightedValue<T>(this T[] weightedArray, float weightedIndex) where T : IWeighted
-        { 
+        public static T GetWeightedValue<T>(this IList<T> weightedArray, float weightedIndex) where T : IWeighted
+        {
             T value = default;
-            
+
             for (int i = 0; weightedIndex > 0; i++) {
                 value = weightedArray[i];
                 weightedIndex -= weightedArray[i].weight;
@@ -64,14 +63,13 @@
             return value;
         }
 
-        
         /// <summary>
         /// Get the sum of a weighted array
         /// </summary>
-        public static float WeightedSum<T>(this T[] weightedArray) where T : IWeighted
+        public static float WeightedSum<T>(this IList<T> weightedArray) where T : IWeighted
         {
             float sum = 0;
-            for (int i = weightedArray.Length; i --> 0;) {
+            for (int i = 0; i < weightedArray.Count; i++) {
                 sum += weightedArray[i].weight;
             }
 
